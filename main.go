f25bbcb7ea27e63c461287ba1ca395a5faf2caa6
Vico4 <victoire.stahl@gmail.com>
2022-05-24 15:17:12 +0200

    route delete done

--- main.go
+++ main.go
@@ -157,33 +157,17 @@ func getList(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
-// func deleteshark(w http.ResponseWriter, r *http.Request) {
-// 	// on récup le Json parsé, comme d'hab 
-// 	parseJson := parsingJson()
-// 	// on récup l'id passé en paramètes
-// 	sharkID := mux.Vars(r)["id"]
+func deleteshark(w http.ResponseWriter, r *http.Request) {
+	
+	sharkID := mux.Vars(r)["id"]
 
-// 	// une boucle for pour chercher le requin concerné
-// 	for i, singleshark := range parseJson.Allsharks {
-// 		if singleshark.ID == sharkID {
-// 			// on supprime le requin concerné en décalant les valeurs du tableau vers la gauche 
-// 			// à partir de l'ID trouvé
-// 			parseJson.Allsharks = append(parseJson.Allsharks[:i], parseJson.Allsharks[i+1:]...)
-// 			fmt.Fprintf(w, "The shark with ID %v has been deleted successfully", sharkID)
-// 		}
-// 	}
+	_, err := db.Exec("DELETE FROM allSharks WHERE id = ?", sharkID)
+	if err != nil {
+		panic(err.Error())
+	}
 
-// 	// afin de pouvoir l'écrire dans le Json, on Marshal notre parseJson 
-// 	modifJson, err := json.Marshal(parseJson)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	// on écrit notre modifJson (parseJson "marshalisé") dans le fichier sharks.json grace à ioutil 
-// 	err = ioutil.WriteFile("sharks.json", modifJson, 0644)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+	fmt.Fprintf(w, "The shark with ID %v has been deleted successfully", sharkID)
+}
 
 var db *sql.DB
 var err error
@@ -203,6 +187,6 @@ func main() {
 	router.HandleFunc("/sharks/{id}", getOneshark).Methods("GET")
 	router.HandleFunc("/sharklist", getList).Methods("GET")
 	// router.HandleFunc("/sharks/{id}", updateshark).Methods("PATCH")
-	// router.HandleFunc("/sharks/{id}", deleteshark).Methods("DELETE")
+	router.HandleFunc("/sharks/{id}", deleteshark).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
